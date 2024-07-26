842096d1e62f045d5fb9d60f4dd621e666e1e722
mozahzah <mozahzah@gmail.com>
2024-07-26 10:26:43 -0500

    Finalizing license header

--- Source/IEMidiEditor.h
+++ Source/IEMidiEditor.h
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-only
-// Copyright © 2024 Immortal Echoes. All rights reserved.
+// Copyright © 2024 Interactive Echoes. All rights reserved.
 // Author: mozahzah
 
 #pragma once
